7b2dd7ef0070b5dd388558f5d023096a917d0501
Shaun W Colley <swc7838@gmail.com>
2019-02-15 15:46:56 -0600

    fixed to find missing element of any sequential set of numbers

--- missing.py
+++ missing.py
@@ -1,12 +1,20 @@
 a = [0,1,2,3,5,6,7,8,9]
-
-def missing_element(array):
-    full_array = list(range(0,10)) # or I could have initialized full array by each element [0,1,2,3,4,5,6,7,8,9]
-    for num in full_array:
-        if num not in array:
-            print(f"Array {array} is missing the following element: {num}")
+#only works for 0-9, function not commented out works for any sequential missing element
+#def missing_element(array):
+#    full_array = list(range(0,10)) # or I could have initialized full array by each element [0,1,2,3,4,5,6,7,8,9]
+#    for num in full_array:
+#        if num not in array:
+#            print(f"Array {array} is missing the following element: {num}")
             #can include below to append array, but places at end, more formating could include
             #array.append(num)
-    return array
+#    return array
 
+
+b = [21,23,24,25,26]
+#find missing element of any array:
+def missing_element(array):
+    for i in range(0, len(array)-1):
+        if (array[i]+1) != array[i+1]:
+            print(f"Missing element is {array[i] + 1}")
 missing_element(a)
+missing_element(b)
