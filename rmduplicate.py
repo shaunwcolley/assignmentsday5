08bb08887a5fd787868233d6a4f10f25c88f3d51
Shaun W Colley <swc7838@gmail.com>
2019-02-15 13:16:24 -0600

    removed comments of previous attempt

--- rmduplicate.py
+++ rmduplicate.py
@@ -1,15 +1,5 @@
 names1 = ["Alex", "John", "Mary", "Steve", "John", "Steve"]
 
-#print(names)
-#The following prints every duplicate name once.
-#for i1 in range(0,len(names1)):
-#    name1 = names1[i1]
-#    for i2 in range(i1+1,len(names1)):
-#        if i2 != i1:
-#            name2 = names1[i2]
-#            if name2 == name1:
-#                names2 += [name]
-#print(names)
 def rmduplicate(names):
     unique_names = []
     for name in names1:
